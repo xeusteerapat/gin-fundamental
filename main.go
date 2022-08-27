9eb057a8cfa2bb6be338201afd1477152beb432a
xeusteerapat <xeus085@gmail.com>
2022-08-27 13:37:46 +0700

    :sparkles: [Update] form data

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"embed"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,8 +28,17 @@ func main() {
 		ctx.File("./public/employee.html")
 	})
 
+	// POST mothod, receive data from Form input
 	router.POST("/employees", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "New POST request success")
+		date := ctx.PostForm("date")
+		amount := ctx.PostForm("amount")
+		username := ctx.DefaultPostForm("username", "Teerapat")
+
+		ctx.IndentedJSON(http.StatusOK, gin.H{
+			"date":     date,
+			"amount":   amount,
+			"username": username,
+		})
 	})
 
 	router.GET("/employees/:username/*rest", func(ctx *gin.Context) {
@@ -65,5 +76,19 @@ func main() {
 		})
 	})
 
-	log.Fatal(router.Run(":3000 🎉"))
+	// Request query
+	// eg. http://localhost:3000/query/?username=xeusteerapat&year=2022&month=8&month=9
+	router.GET("/query/*rest", func(ctx *gin.Context) {
+		username := ctx.Query("username")
+		year := ctx.DefaultQuery("year", strconv.Itoa(time.Now().Year()))
+		months := ctx.QueryArray("month")
+
+		ctx.IndentedJSON(http.StatusOK, gin.H{
+			"username": username,
+			"year":     year,
+			"months":   months,
+		})
+	})
+
+	log.Fatal(router.Run(":3000"))
 }
